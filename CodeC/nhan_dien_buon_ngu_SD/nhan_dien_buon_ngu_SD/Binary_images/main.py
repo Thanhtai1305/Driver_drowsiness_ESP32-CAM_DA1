892396b258d75a9ca2874cd7bccae5ad50139806
Tran Nguyen Thanh Tai <thanhtait4025@gmail.com>
2025-09-11 00:22:38 +0700

    feat(develop): Remove folder nhan_dien_buon_ngu_SD

--- CodeC/nhan_dien_buon_ngu_SD/nhan_dien_buon_ngu_SD/Binary_images/main.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-
-img = cv2.imread("./image/image_8610.pgm", cv2.IMREAD_GRAYSCALE)
-plt.imshow(img, cmap='gray')
-plt.title("PGM Image")
-plt.axis("off")
-plt.show()
